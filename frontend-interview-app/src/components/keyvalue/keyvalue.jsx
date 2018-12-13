942925ab582a00c1797cac5917b941971b286e53
Ryan-Milton <mr.ryan.milton@gmail.com>
2018-12-12 21:31:54 -0800

    can now duplicate KeyValue forms

--- frontend-interview-app/src/components/keyvalue/keyvalue.jsx
+++ frontend-interview-app/src/components/keyvalue/keyvalue.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const KeyValue = (props) => {
   return (
     <React.Fragment>
-      <form>
+      <form id={props.id}>
         <input type='text' placeholder='key'/>
         <input type='text' placeholder='value'/>
         <button onClick={props.delete}>x</button>

ca160b4387afb868f828d2ad220c571afa71612b
Ryan-Milton <mr.ryan.milton@gmail.com>
2018-12-15 11:17:46 -0800

    saving current prorgress, converting to MobX state

--- frontend-interview-app/src/components/keyvalue/keyvalue.jsx
+++ frontend-interview-app/src/components/keyvalue/keyvalue.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 const KeyValue = (props) => {
   return (
     <React.Fragment>
-      <form id={props.id}>
-        <input type='text' placeholder='key'/>
-        <input type='text' placeholder='value'/>
-        <button onClick={props.delete}>x</button>
+      <form id={props.id} onSubmit={props.delete}>
+        <input type='text' placeholder='key' onChange={props.change}/>
+        <input type='text' placeholder='value' onChange={props.change}/>
+        <button type='submit'>x</button>
       </form>
     </React.Fragment>
   );
